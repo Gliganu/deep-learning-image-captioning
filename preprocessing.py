ae3eae17d0010daf9318007c61551fe487985678
Gliganu <gligabogdanh@gmail.com>
2017-03-13 13:29:05 +0200

    Split MS COCO preprocessing in separate py file

---
+++ preprocessing.py
@@ -0,0 +1,159 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD, Adadelta, Adagrad
+from keras.utils import np_utils, generic_utils
+from keras.callbacks import EarlyStopping
+from keras.layers.advanced_activations import PReLU, LeakyReLU
+from keras.layers import Embedding,GRU,TimeDistributed,RepeatVector,Merge
+from keras.preprocessing.text import one_hot
+from keras.preprocessing import sequence
+import cv2
+import numpy as np
+from vgg16 import Vgg16
+
+from os import listdir
+from os.path import isfile, join
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import PIL.Image
+
+import json
+from tqdm import tqdm
+
+from keras.optimizers import SGD, RMSprop, Adam
+
+from utils import *
+import cPickle as pickle
+from matplotlib import pyplot as plt
+
+from itertools import compress
+
+import shutil
+import string
+
+import collections
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+
+
+class ImageData(object):
+
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+        self.captions = []
+        self.image = []
+        
+    def appendCaption(self,caption):
+        self.captions.append(caption)
+        
+class ImageEntry(object):
+
+    def __init__(self,image,caption):
+        self.image = image
+        self.caption = caption
+        
+              
+def construct_image_data_arr(base_path,fileName2ImageDataDict):   
+    
+    image_paths = [f for f in listdir(base_path)]
+
+    
+    for image_file_name in tqdm(image_paths):
+        
+        img = PIL.Image.open(base_path+"/"+image_file_name)
+        img = img.resize((224, 224), PIL.Image.NEAREST)
+        
+        image_data = fileName2ImageDataDict[image_file_name]
+        
+        img = np.asarray(img)
+        
+        image_data.image = img
+        image_data.image = np.asarray(image_data.image)
+        
+        
+        
+    all_image_data = [imageData for _,imageData in fileName2ImageDataDict.iteritems()]
+    
+    filtered_image_data = [imageData for imageData in all_image_data
+                      if np.asarray(imageData.image).shape == (224,224,3)]
+    
+
+    return  filtered_image_data
+
+
+
+def build_data_dict(annotation_path):
+    
+    with open(annotation_path) as data_file:    
+        data = json.load(data_file)
+        
+    id2ImageDataDict = {imageJson["id"]: ImageData(imageJson["id"],imageJson["file_name"]) 
+                        for imageJson in data["images"]}
+    
+    annotationsJson = data["annotations"]
+    
+    for annotationJson in annotationsJson:
+        imageData = id2ImageDataDict[annotationJson["image_id"]]
+        caption = annotationJson["caption"]
+        imageData.appendCaption(caption)
+
+    fileName2ImageDataDict = {imageJson["file_name"]: id2ImageDataDict[imageJson["id"]] for imageJson in data["images"]}
+
+    return fileName2ImageDataDict
+
+
+def get_train_test_data(image_data_arr, test_size):
+    train_image_data_arr = image_data_arr[test_size:]
+    test_image_data_arr = image_data_arr[:test_size]
+    return (train_image_data_arr,test_image_data_arr)
+    
+    
+
+def get_image_data_arr(images_path,annotation_path):
+    fileName_2_image_data_dict = build_data_dict(annotation_path)
+    image_data_arr = construct_image_data_arr(images_path,fileName_2_image_data_dict)
+    return image_data_arr
+
+
+
+def construct_images_concat_t(image_data_arr):
+    image_np_arr = [ np.expand_dims(image_data.image, axis=0) for image_data in image_data_arr]
+    images_concat =  np.vstack(image_np_arr)
+    images_concat_t = np.transpose(images_concat,(0,3,1,2))
+    return images_concat_t
+
+
+def write_captions_to_disk(path,image_data_arr):
+    for i in tqdm(range(5)):
+        captions = ["START "+image_data.captions[i]+" END" for image_data in image_data_arr] 
+        pickle.dump( captions, open(path+"captions_batch_"+str(i)+".p", "wb" ) )
+        
+        
+def read_serialized_np_arr(path,nr_instances = None):
+    images_concat_t = load_array(path)
+    if(nr_instances):
+        return images_concat_t[:nr_instances]
+    
+    return images_concat_t
+
+    
+def get_captions_from_batch(path,batch_nr):
+    return pickle.load(open(path+"captions_batch_"+str(batch_nr)+".p", "rb" ))
+
+def get_truncated_captions_from_batch(path,batch_nr,nr_instances):
+    captions = get_captions_from_batch(path,batch_nr)
+    return captions[:nr_instances]
+
+
+
+        
+
+
+
+
